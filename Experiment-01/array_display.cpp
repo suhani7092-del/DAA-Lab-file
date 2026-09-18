22eaae3c8f53ca5bb41608fca2ebe202fcd08cdf
suhani7092-del <suhani7092@gmail.com>
2026-09-18 10:28:38 +0530

    Add array_display.cpp to display array elements

---
+++ Experiment-01/array_display.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    cout << "The elements of the array are: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
